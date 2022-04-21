f6ed04e7ea766da01a43ee3502c89534ef377815
GorkenIzmir <gokkus_17@hotmail.com>
2022-04-21 18:31:06 +0300

    18,30-2104

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,38 +5,34 @@
 				<div class="col-md-3 infor-left">
 					<h3>Follow Us</h3>
 					<ul>
-						<li><a href="#"><span class="fb"></span><h6>Facebook</h6></a></li>
-						<li><a href="#"><span class="twit"></span><h6>Twitter</h6></a></li>
-						<li><a href="#"><span class="google"></span><h6>Google+</h6></a></li>
+						<li><a href="{{ url('https://saglamnakliyat.com')}}"><span class="fb"></span><h6>Facebook</h6></a></li>
+						<li><a href="{{ url('https://saglamnakliyat.com')}}"><span class="twit"></span><h6>Twitter</h6></a></li>
+						<li><a href="{{ url('https://saglamnakliyat.com')}}"><span class="google"></span><h6>Google+</h6></a></li>
 					</ul>
 				</div>
 				<div class="col-md-3 infor-left">
 					<h3>Information</h3>
 					<ul>
-						<li><a href="#"><p>Specials</p></a></li>
-						<li><a href="#"><p>New Products</p></a></li>
-						<li><a href="#"><p>Our Stores</p></a></li>
-						<li><a href="contact"><p>Contact Us</p></a></li>
-						<li><a href="#"><p>Top Sellers</p></a></li>
+						<li><a href="{{route("products")}}"><p>New Products</p></a></li>
+						<li><a href="{{route("products")}}"><p>Our Stores</p></a></li>
+						<li><a href="{{ url('https://saglamnakliyat.com')}}"><p>Contact Us</p></a></li>
+						<li><a href="{{route("products")}}"><p>Top Sellers</p></a></li>
 					</ul>
 				</div>
 				<div class="col-md-3 infor-left">
-					<h3>My Account</h3>
+					<h3>This Web Site</h3>
 					<ul>
-						<li><a href="account"><p>My Account</p></a></li>
-						<li><a href="#"><p>My Credit slips</p></a></li>
-						<li><a href="#"><p>My Merchandise returns</p></a></li>
-						<li><a href="#"><p>My Personal info</p></a></li>
-						<li><a href="#"><p>My Addresses</p></a></li>
+						<li><a href="{{ url('https://saglamnakliyat.com')}}"><p>My Web Site</p></a></li>
+						
 					</ul>
 				</div>
 				<div class="col-md-3 infor-left">
 					<h3>Store Information</h3>
-					<h4>The company name,
-						<span>Lorem ipsum dolor,</span>
-						Glasglow Dr 40 Fe 72.</h4>
-					<h5>+955 123 4567</h5>
-					<p><a href="mailto:example@email.com">contact@example.com</a></p>
+					<h4>Türkiye
+						<span>Kocaeli/İzmit</span>
+						Umuttepe Kampüs</h4>
+					<h5>+90 555 555 55 55</h5>
+					<p><a href="mailto:example@email.com">hello@example.com</a></p> 
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -54,7 +50,7 @@
 					</form>
 				</div>
 				<div class="col-md-6 footer-right">
-					<p>© 2022 İZMİR Saatleri. Tüm hakları Saklıdır | Design by  <a href="https://www.linkedin.com/in/g%C3%B6rken-izmir-462664199" target="_blank">İZMİR SAAT</a> </p>
+					<p>© 2022 İZMİR Saatleri. Tüm hakları Saklıdır | Design by  <a href="{{ url('https://saglamnakliyat.com')}}" target="_blank">İZMİR SAAT</a> </p>
 				</div>
 				<div class="clearfix"></div>
 			</div>
